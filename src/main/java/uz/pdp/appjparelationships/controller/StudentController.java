6ec81339a9e35bd0402780a02a1669686e0a5319
murodleave <murodleave@gmail.com>
2023-02-19 03:59:53 +0500

    univer

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -14,35 +14,32 @@ public class StudentController {
     @Autowired
     StudentRepository studentRepository;
 
-    //1. VAZIRLIK
     @GetMapping("/forMinistry")
     public Page<Student> getStudentListForMinistry(@RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
         Pageable pageable = PageRequest.of(page, 10);
-        Page<Student> studentPage = studentRepository.findAll(pageable);
-        return studentPage;
+        return studentRepository.findAll(pageable);
     }
 
-    //2. UNIVERSITY
     @GetMapping("/forUniversity/{universityId}")
     public Page<Student> getStudentListForUniversity(@PathVariable Integer universityId,
                                                      @RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
+
         Pageable pageable = PageRequest.of(page, 10);
-        Page<Student> studentPage = studentRepository.findAllByGroup_Faculty_UniversityId(universityId, pageable);
-        return studentPage;
+        return studentRepository.findAllByGroup_Faculty_UniversityId(universityId, pageable);
     }
 
-    //3. FACULTY DEKANAT
-    //4. GROUP OWNER
+
+    @GetMapping("/forFaculty/{facultyId}")
+    public Page<Student> getStudentListForFaculty(@PathVariable Integer facultyId, @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return studentRepository.findAllByGroup_FacultyId(facultyId, pageable);
+    }
+
+    @GetMapping("/forGroup/{groupId}")
+    public Page<Student> getStudentListForGroup(@PathVariable Integer groupId, @RequestParam int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return studentRepository.findAllByGroupId(groupId, pageable);
+    }
 
 
 }
